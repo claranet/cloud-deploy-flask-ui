9b7a3e71f0b2103765b1c9969daedaa22e077758
Jérôme Respaut <shr3ps@gmail.com>
2015-11-19 12:23:47 +0100

    GHOST-132: Show only usefull fields

--- forms.py
+++ forms.py
@@ -658,9 +658,9 @@ class EditAppForm(BaseAppForm):
 
 class CommandAppForm(Form):
     command = SelectField('Command', validators=[DataRequiredValidator()], choices=get_ghost_job_commands())
-    module_name = SelectField('Module name<br>(deploy only)', validators=[])
-    module_rev = StringField('Module revision<br>(deploy only)', validators=[])
-    module_deploy_id = StringField('Deploy ID<br>(rollback only)', validators=[])
+    module_name = SelectField('Module name', validators=[])
+    module_rev = StringField('Module revision', validators=[])
+    module_deploy_id = StringField('Deploy ID', validators=[])
 
     submit = SubmitField('Run Application Command')
 
