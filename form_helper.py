e6196e3a54ce63ba3f1152b3be6dc1f9752fb100
Jérôme Respaut <shr3ps@gmail.com>
2016-08-03 16:47:48 +0200

    CRLF -> LF

--- form_helper.py
+++ form_helper.py
@@ -18,7 +18,7 @@ class BetterSelectField(SelectField):
         self.choices = copy(choices)
 
     def iter_choices(self):
-        if self.choices and not self.data in [k for k,v in self.choices]:
+        if self.choices and self.data and not self.data in [k for k,v in self.choices]:
             self.choices = self.choices + [(self.data, u'⚠ "%s" not available in choices ⚠' % self.data)]
         for value, label in self.choices:
             yield (value, label, self.coerce(value) == self.data)
