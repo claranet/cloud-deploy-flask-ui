03299aaa521e811ebab9f5ba360263581955b311
Jérôme Respaut <shr3ps@gmail.com>
2016-11-09 14:15:22 +0100

    GHOST-307: Tabulation app list view

--- app.py
+++ app.py
@@ -255,15 +255,13 @@ def web_app_list():
 @app.route('/web/t-apps')
 def web_tab_app_list():
     choosen_env = request.args.get('env', None)
-    envs = get_ghost_envs()
     if not choosen_env:
+        envs = get_ghost_envs()
         choosen_env = list(envs)[0]
     apps = get_ghost_apps_per_env(choosen_env)
     if request.is_xhr:
-        return render_template('app_list_content.html', apps=apps,
-                               page=0)
-    return render_template('app_list.html', apps=apps,
-                           page=0)
+        return render_template('app_tab_content.html', apps=apps)
+    return render_template('app_tab.html', apps=apps, choosen_env=choosen_env)
 
 @app.route('/web/apps/create', methods=['GET', 'POST'])
 def web_app_create():
