9a2105fd46d32ac249804ab411384dda004ff28c
Jérôme Respaut <shr3ps@gmail.com>
2016-11-21 11:08:12 +0100

    GHOST-291: Handle if there is one or two options for `createinstance`

--- app.py
+++ app.py
@@ -491,10 +491,11 @@ def web_app_command_from_job(app_id, job_id):
     if job['command'] == 'redeploy' and 'options' in job and len(job['options']):
         form.deploy_id.data = job['options'][0]
 
-    if job['command'] == 'createinstance' and 'options' in job and len(job['options']) > 1:
+    if job['command'] == 'createinstance' and 'options' in job and len(job['options']) > 0:
         form.subnet.choices = get_aws_subnets_ids_from_app(DEFAULT_PROVIDER, app['region'], app['environment_infos']['subnet_ids'], **aws_connection_data)
         form.subnet.data = job['options'][0]
-        form.private_ip_address.data = job['options'][1]
+        if len(job['options']) > 1:
+            form.private_ip_address.data = job['options'][1]
 
     return render_template('app_command.html', form=form, app=app)
 
