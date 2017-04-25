64f837b97fc19c1eaa6f7d24ebc5dd81fa0469d3
baptiste <bbenet@morea.Fr>
2017-04-25 15:33:38 +0200

    GHOST-73: Change Default choice on containers select button

--- forms/app.py
+++ forms/app.py
@@ -990,7 +990,7 @@ class CreateAppForm(BaseAppForm):
         self.environment_infos.instance_profile.choices = [('', 'Please select region first')]
         self.environment_infos.key_name.choices = [('', 'Please select region first')]
         self.build_infos.source_ami.choices = [('', 'Please select region first')]
-        self.build_infos.container.choices = [('', 'Not use container')]
+        self.build_infos.container.choices = [('', 'No container used')]
         self.build_infos.subnet_id.choices = [('', 'Please select VPC first')]
         self.environment_infos.subnet_ids[0].choices = [('', 'Please select VPC first')]
 
