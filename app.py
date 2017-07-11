f248228e243c02229905d55390269e90bcbcb274
Jérôme Respaut <shr3ps@gmail.com>
2017-07-11 11:08:12 +0200

    GHOST-431: No more need to specify scheme and generate absolute links

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ def web_feature_presets_import(config):
 @app.route('/web/t-apps')
 def web_t_apps_list():
     #redirect to apps as the page was unified
-    return redirect(url_for('web_app_list', _scheme=request.scheme, _external=True), code=301)
+    return redirect(url_for('web_app_list'), code=301)
 
 @app.route('/web/apps')
 def web_app_list():
@@ -632,7 +632,7 @@ def web_app_delete(app_id):
         return render_template('action_completed.html', message=message, form_action='delete')
     elif form.validate_on_submit and form.confirmation.data == 'no':
         flash('App "%s" has not been deleted' % app_id, 'info')
-        return redirect(url_for('web_app_list', _scheme=request.scheme, _external=True), code=301)
+        return redirect(url_for('web_app_list'), code=301)
 
     # Get Application etag
     app = get_ghost_app(app_id)
@@ -683,7 +683,7 @@ def web_job_delete(job_id):
         return render_template('action_completed.html', message=message, form_action='delete')
     elif form.validate_on_submit and form.confirmation.data == 'no':
         flash('Job "%s" has not been deleted' % job_id, 'info')
-        return redirect(url_for('web_job_list', _scheme=request.scheme, _external=True), code=301)
+        return redirect(url_for('web_job_list'), code=301)
 
     # Get job etag
     job = get_ghost_job(job_id)
